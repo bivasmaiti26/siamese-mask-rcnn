cabc575a15e60dc356cf9501b46d76f3da9ff159
bivasmaiti26 <bivasmaiti26@gmail.com>
2019-07-18 23:21:46 -0400

    load ycb check dataset paths

--- lib/Mask_RCNN/samples/ycb/ycb.py
+++ lib/Mask_RCNN/samples/ycb/ycb.py
@@ -4,7 +4,7 @@ import math
 import random
 import numpy as np
 import cv2
-
+import fnmatch
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
 
@@ -14,24 +14,39 @@ from mrcnn.config import Config
 from mrcnn import utils
 
 class YCBDataset(utils.Dataset):
-     """Generates the YCB dataset. The Dataset needs to be present in the root folder of 
-     the repository. For example: /root_dir/YCB_Video_Dataset/data
     """
-    def load_ycb(self, count):
+
+    """
+    def load_ycb(self, count=0):
         """Generate the requested number of images from the dataset.
-        count: number of images to generate.
-        height, width: the size of the generated images.
+        count: number of images to generate. If set to 0, get all images
         """
-        pass
-     def load_image(self, image_id):
-         """Load the image from the file with the image id
+    print(os.getcwd())
+    dataset_folder = '/ycb/data'
+    root_folders = os.listdir(dataset_folder)
+    i = 0
+    for folder in root_folders:
+        files = os.listdir(dataset_folder+'/'+ folder)
+        for file in files:
+            if fnmatch.fnmatch(file, '*color*'):
+                i+=1
+                if i > count:
+                    break
+                image_file_id = file.split('-')[0]
+                self.add_image("ycb", image_id = i, path = folder + '/' + image_file_id) 
+        if i > count:
+            break
+         
+    def load_image(self, image_id):
+        """Load the image from the file with the image id
         """
-        pass
+        
+        
     def load_mask(self, image_id):
         """Generate instance masks for the image of the given image ID.
         """
         pass
-     def image_reference(self, image_id):
+    def image_reference(self, image_id):
         """Return the shapes data of the image."""
         info = self.image_info[image_id]
         if info["source"] == "shapes":
