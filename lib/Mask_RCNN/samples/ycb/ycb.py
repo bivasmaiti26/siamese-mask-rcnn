5a739f189955c0f487db1d6e602d2bd0f75fa15e
bivasmaiti26 <bivasmaiti26@gmail.com>
2019-07-19 00:07:15 -0400

    add load image and mask function

--- lib/Mask_RCNN/samples/ycb/ycb.py
+++ lib/Mask_RCNN/samples/ycb/ycb.py
@@ -12,7 +12,7 @@ ROOT_DIR = os.path.abspath("../../")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
 from mrcnn import utils
-
+dataset_folder = 'ycb/data'
 class YCBDataset(utils.Dataset):
     """
 
@@ -20,9 +20,8 @@ class YCBDataset(utils.Dataset):
     def load_ycb(self, count_images = 0):
         """Generate the requested number of images from the dataset.
         count: number of images to generate. If set to 0, get all images
+        TODO-Add more functionality here later
         """
-        print(os.getcwd())
-        dataset_folder = 'ycb/data'
         root_folders = os.listdir(dataset_folder)
         i = 0
         for folder in root_folders:
@@ -40,12 +39,20 @@ class YCBDataset(utils.Dataset):
     def load_image(self, image_id):
         """Load the image from the file with the image id
         """
-        
+        image = cv2.imread(dataset_folder+'/'+self.image_info[image_id]['path']+'-color.png')
+        return image
         
     def load_mask(self, image_id):
         """Generate instance masks for the image of the given image ID.
         """
-        pass
+        mask_image = cv2.imread(dataset_folder+'/'+dataset_ycb.image_info[1]['path']+'-label.png')[:,:,0]  
+        classes = np.unique(mask_image)
+        mask = np.zeros([480, 640,len(classes)], dtype=np.uint8)
+        i = 0
+        for obj_class in classes and obj_class>0:
+            mask[:,:,i] = mask_image == obj_class
+            i += 1
+
     def image_reference(self, image_id):
         """Return the shapes data of the image."""
         info = self.image_info[image_id]
